266abea9303d7081bde903fb098b580c52af1406
Cong Nhut <dgxa9999@gmail.com>
2023-09-10 21:37:59 +0700

    .

--- src/components/scroll-to-stop/index.tsx
+++ src/components/scroll-to-stop/index.tsx
@@ -2,14 +2,14 @@ import React, {PropsWithChildren, useEffect} from 'react'
 import { useLocation } from 'react-router-dom'
 
 function ScrollToStop({children}: PropsWithChildren) {
-  const location = useLocation()
+  const location = useLocation() // thay đổi url
   useEffect(() =>{
-    document.documentElement.scrollTo({
+    document.documentElement.scrollTo({ //scroll to top
       top: 0,
       left: 0,
       behavior: 'smooth'
     })
-  }, [location])
+  }, [location]) // thay đổi url
 
   return <>{children}</>
     
