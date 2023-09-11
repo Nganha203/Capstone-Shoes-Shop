307c49b31806716a70adf37eb53786db97e715c8
Nganha203 <nguyenngocnganha203@gmail.com>
2023-09-11 18:09:29 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Capstone-Shoes-Shop",
   plugins: [react()],
   server: {
     port: 7000
