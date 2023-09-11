2edb9c5e1f04738d9de8e991ab2edfe5b780c8cd
Nganha203 <nguyenngocnganha203@gmail.com>
2023-09-11 17:49:01 +0700

    .

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -9,7 +9,7 @@ export const userLogin = async (data: { email: string; password: string }) => {
             data,
         });
         return resp.data;
-    } catch (error) {   
+    } catch (error:any) {   
         console.log(error)
         return error.response.data.message;
     }
@@ -22,7 +22,7 @@ export const signup = async (data: UserRegister) => {
             data,
         });
         return resp.data;
-    } catch (err) {
+    } catch (err:any) {
         console.log(err)
         return err.response.data.message;
     }
